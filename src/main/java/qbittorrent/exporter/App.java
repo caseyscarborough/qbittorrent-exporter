5aa0f95c3b68acad190ef89bf32c21bff5d106ac
Casey Scarborough <caseyscarborough@gmail.com>
2020-02-18 19:08:56 -0500

    Add total dl and up speed metrics

--- src/main/java/qbittorrent/exporter/App.java
+++ src/main/java/qbittorrent/exporter/App.java
@@ -21,6 +21,7 @@ public class App {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 
+    private static final String GAUGE_NAME_PREFIX = "qbittorrent_";
     private static final String USERNAME_ENV_KEY = "QBITTORRENT_USERNAME";
     private static final String PASSWORD_ENV_KEY = "QBITTORRENT_PASSWORD";
     private static final String HOST_ENV_KEY = "QBITTORRENT_HOST";
@@ -31,7 +32,7 @@ public class App {
     private static final String DEFAULT_PORT = "8080";
     private static final int METRICS_PORT = 17871;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String username = System.getenv(USERNAME_ENV_KEY);
         String password = System.getenv(PASSWORD_ENV_KEY);
         String host = System.getenv(HOST_ENV_KEY);
@@ -59,127 +60,138 @@ public class App {
 
         final ApiClient client = new ApiClient(host, port);
         client.login(username, password);
+        LOGGER.info("Your qBittorrent version is " + client.getVersion() + ".");
+
+        Gauge dlSpeedTotal = Gauge.build()
+            .name(GAUGE_NAME_PREFIX + "download_speed_bytes_total")
+            .help("The total current download speed of all torrents (in bytes)")
+            .create();
+
+        Gauge upSpeedTotal = Gauge.build()
+            .name(GAUGE_NAME_PREFIX + "upload_speed_bytes_total")
+            .help("The total current upload speed of all torrents (in bytes)")
+            .create();
 
         List<Gauge> gauges = new ArrayList<>();
         Gauge dlSpeed = Gauge.build()
-            .name("qbittorrent_download_speed_bytes")
+            .name(GAUGE_NAME_PREFIX + "download_speed_bytes")
             .labelNames("name")
             .help("The current download speed of torrents (in bytes)")
             .create();
         gauges.add(dlSpeed);
 
         Gauge upSpeed = Gauge.build()
-            .name("qbittorrent_upload_speed_bytes")
+            .name(GAUGE_NAME_PREFIX + "upload_speed_bytes")
             .labelNames("name")
             .help("The current upload speed of torrents (in bytes)")
             .create();
         gauges.add(upSpeed);
 
         Gauge progress = Gauge.build()
-            .name("qbittorrent_progress")
+            .name(GAUGE_NAME_PREFIX + "progress")
             .labelNames("name")
             .help("The current progress of torrents")
             .create();
         gauges.add(progress);
 
         Gauge downloadedBytesTotal = Gauge.build()
-            .name("qbittorrent_downloaded_bytes_total")
+            .name(GAUGE_NAME_PREFIX + "downloaded_bytes_total")
             .labelNames("name")
             .help("The current total download amount of torrents (in bytes)")
             .create();
         gauges.add(downloadedBytesTotal);
 
         Gauge downloadedBytesSession = Gauge.build()
-            .name("qbittorrent_downloaded_bytes_session")
+            .name(GAUGE_NAME_PREFIX + "downloaded_bytes_session")
             .labelNames("name")
             .help("The current session download amount of torrents (in bytes)")
             .create();
         gauges.add(downloadedBytesSession);
 
         Gauge uploadedBytesTotal = Gauge.build()
-            .name("qbittorrent_uploaded_bytes_total")
+            .name(GAUGE_NAME_PREFIX + "uploaded_bytes_total")
             .labelNames("name")
             .help("The current total upload amount of torrents (in bytes)")
             .create();
         gauges.add(uploadedBytesTotal);
 
         Gauge uploadedBytesSession = Gauge.build()
-            .name("qbittorrent_uploaded_bytes_session")
+            .name(GAUGE_NAME_PREFIX + "uploaded_bytes_session")
             .labelNames("name")
             .help("The current session upload amount of torrents (in bytes)")
             .create();
         gauges.add(uploadedBytesSession);
 
         Gauge timeActive = Gauge.build()
-            .name("qbittorrent_time_active")
+            .name(GAUGE_NAME_PREFIX + "time_active")
             .labelNames("name")
             .help("The total active time (in seconds)")
             .create();
         gauges.add(timeActive);
 
         Gauge state = Gauge.build()
-            .name("qbittorrent_state")
+            .name(GAUGE_NAME_PREFIX + "state")
             .labelNames("name")
             .help("The current state of torrents")
             .create();
         gauges.add(state);
 
         Gauge version = Gauge.build()
-            .name("qbittorrent_version")
+            .name(GAUGE_NAME_PREFIX + "version")
             .labelNames("version")
             .help("The current qBittorrent version")
             .create();
         gauges.add(version);
 
         Gauge seeders = Gauge.build()
-            .name("qbittorrent_seeders")
+            .name(GAUGE_NAME_PREFIX + "seeders")
             .labelNames("name")
             .help("The current number of seeders for each torrent")
             .create();
         gauges.add(seeders);
 
         Gauge leechers = Gauge.build()
-            .name("qbittorrent_leechers")
+            .name(GAUGE_NAME_PREFIX + "leechers")
             .labelNames("name")
             .help("The current number of leechers for each torrent")
             .create();
         gauges.add(leechers);
 
         Gauge ratio = Gauge.build()
-            .name("qbittorrent_ratio")
+            .name(GAUGE_NAME_PREFIX + "ratio")
             .labelNames("name")
             .help("The current ratio each torrent")
             .create();
         gauges.add(ratio);
 
         Gauge amountLeft = Gauge.build()
-            .name("qbittorrent_amount_left_bytes")
+            .name(GAUGE_NAME_PREFIX + "amount_left_bytes")
             .labelNames("name")
             .help("The amount remaining for each torrent (in bytes)")
             .create();
         gauges.add(amountLeft);
 
         Gauge size = Gauge.build()
-            .name("qbittorrent_size_bytes")
+            .name(GAUGE_NAME_PREFIX + "size_bytes")
             .labelNames("name")
             .help("The size for each torrent (in bytes)")
             .create();
         gauges.add(size);
 
         Gauge totalTorrents = Gauge.build()
-            .name("qbittorrent_total_torrents")
+            .name(GAUGE_NAME_PREFIX + "total_torrents")
             .help("The total number of torrents")
             .create();
         gauges.add(totalTorrents);
 
         Gauge info = Gauge.build()
-            .name("qbittorrent_info")
+            .name(GAUGE_NAME_PREFIX + "info")
             // Labels are named this way for displaying them in a table with Grafana.
             // Columns can't be ordered and are ordered by name when using Prometheus
             // as a datasource. You can override the column names in the table Column
             // Style configuration.
             // see https://github.com/grafana/grafana/issues/5082
-            .labelNames("_01_name", "_08_state", "_07_size", "_02_progress", "_05_seeders", "_06_leechers", "_03_dl_speed", "_04_up_speed")
+            .labelNames("_01_name", "_08_state", "_07_size", "_02_progress", "_05_seeders", "_06_leechers", "_03_dl_speed", "_04_up_speed", "_09_amount_left", "_10_time_active")
             .help("All info for torrents")
             .create();
         gauges.add(info);
@@ -203,6 +215,7 @@ public class App {
 
                     List<Torrent> torrents = client.getTorrents();
                     totalTorrents.set(torrents.size());
+
                     for (Torrent torrent : torrents) {
                         dlSpeed.labels(torrent.getName()).set(torrent.getDlspeed());
                         upSpeed.labels(torrent.getName()).set(torrent.getUpspeed());
@@ -225,8 +238,13 @@ public class App {
                             String.valueOf(torrent.getNumSeeds()),
                             String.valueOf(torrent.getNumLeechs()),
                             String.valueOf(torrent.getDlspeed()),
-                            String.valueOf(torrent.getUpspeed())).set(1);
+                            String.valueOf(torrent.getUpspeed()),
+                            String.valueOf(torrent.getAmountLeft()),
+                            String.valueOf(torrent.getTimeActive())).set(1);
                     }
+
+                    dlSpeedTotal.set(torrents.stream().mapToDouble(Torrent::getDlspeed).sum());
+                    upSpeedTotal.set(torrents.stream().mapToDouble(Torrent::getUpspeed).sum());
 
                     List<String> states = torrents.stream().map(Torrent::getState).distinct().collect(Collectors.toList());
                     for (String s : states) {
